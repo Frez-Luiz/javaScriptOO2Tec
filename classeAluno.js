7acd5019f9a698d874bcaa1faa69b53cdf7b7c6e
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2026-04-13 18:00:19 +0000

    classe aluno 02

--- classeAluno.js
+++ classeAluno.js
@@ -1,17 +1,36 @@
-class Aluno{
-    constructor(nome, matricula, notas){
+class Aluno {
+    constructor(nome, matricula, notas) {
         this.nome = nome;
         this.matricula = matricula;
         this.notas = notas;
 
     }
-    calcularMedia(){
+    calcularMedia() {
         let soma = 0;
-        for (let i = 0; i < this.notas.length; i++){
+        for (let i = 0; i < this.notas.length; i++) {
             soma += this.notas[i];
         }
         return soma / this.notas.length;
     }
+    obterStatus() {
+        const media = this.calcularMedia()
+
+        if (media >= 7) {
+            return "Aprovado"
+        } else if (media >= 5) {
+            return "Recuperação"
+        } else {
+            return "Reprovado"
+        }
+    }
+    exibirRelatorio(){
+        console.log(`Relatório do aluno ${this.nome}`);
+        console.log(`Mátricula: ${this.matricula}`);
+        console.log(`Média Final ${this.calcularMedia().toFixed(2)}`);
+        console.log(`Status ${this.obterStatus()}`);
+    }
 }
-let aluno1 = new Aluno ("Ana", 5500, [8.0, 7.3, 9.0]);
-console.log (aluno1.calcularMedia())
+let aluno1 = new Aluno("Luiz Felipe", 5500, [8.0, 7.3, 9.0]);
+console.log(aluno1.calcularMedia());
+console.log(aluno1.obterStatus());
+aluno1.exibirRelatorio()
